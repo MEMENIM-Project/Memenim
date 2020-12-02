12ba10eaa4ddaa9d7fb00f3aa677672f4d22ffa7
smbdy <shadows2001@gmail.com>
2020-12-03 00:16:19 +0200

    Features: - #175 progress; - #174 progress

---
+++ Widgets/PostStatus.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Memenim.Widgets
+{
+    
+    public enum EPostStatus
+    {
+        Premoderating = 0,
+        Published = 1,
+        Rejected = 2
+    }
+
+    public partial class PostStatus : UserControl
+    {
+        public static readonly DependencyProperty StatusValueProperty =
+                DependencyProperty.Register(nameof(StatusValue), typeof(EPostStatus), typeof(PostStatus),
+                    new PropertyMetadata(EPostStatus.Premoderating));
+
+
+        public EPostStatus StatusValue 
+        { 
+            get
+            {
+                return (EPostStatus)GetValue(StatusValueProperty);
+            }
+            set
+            {
+                SetValue(StatusValueProperty, value);
+            }
+        }
+
+        public PostStatus()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
---
+++ Widgets/UserStatus.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Memenim.Widgets
+{
+    /// <summary>
+    /// Interaction logic for UserStatus.xaml
+    /// </summary>
+    /// 
+
+    public enum EUserStatus
+    {
+        Active = 0,
+        Banned = 2,
+        Moderator = 9,
+        Admin = 10
+    }
+
+    public partial class UserStatus : UserControl
+    {
+        public static readonly DependencyProperty StatusValueProperty =
+            DependencyProperty.Register(nameof(StatusValue), typeof(EUserStatus), typeof(UserStatus),
+                new PropertyMetadata(EUserStatus.Active));
+
+        public EUserStatus StatusValue
+        {
+            get 
+            {
+                return (EUserStatus)GetValue(StatusValueProperty);
+            }
+            set 
+            {
+                SetValue(StatusValueProperty, value);
+            }
+        }
+
+        public UserStatus()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
