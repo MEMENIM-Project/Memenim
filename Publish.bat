460fc0f375df8513c73a1cd67e477c193b610555
Daerius-rs <dima.armawir@yandex.ru>
2021-07-16 18:31:00 +0300

    Development:
    - Change echo mode for Publish.bat file

--- Publish.bat
+++ Publish.bat
@@ -1,19 +1,21 @@
-echo win-x64 section
+@ECHO OFF
+
+ECHO win-x64 section
 
 dotnet publish "Memenim.csproj" -c "Release" -p:PublishProfile="netcore3.1 win-x64 standalone.pubxml"
 dotnet publish "Memenim.csproj" -c "Release" -p:PublishProfile="netcore3.1 win-x64.pubxml"
 
-echo win-x64 no-single-file section
+ECHO win-x64 no-single-file section
 
 dotnet publish "Memenim.csproj" -c "Release" -p:PublishProfile="netcore3.1 win-x64 standalone no-single-file.pubxml"
 dotnet publish "Memenim.csproj" -c "Release" -p:PublishProfile="netcore3.1 win-x64 no-single-file.pubxml"
 
-echo win-x86 section
+ECHO win-x86 section
 
 dotnet publish "Memenim.csproj" -c "Release" -p:PublishProfile="netcore3.1 win-x86 standalone.pubxml"
 dotnet publish "Memenim.csproj" -c "Release" -p:PublishProfile="netcore3.1 win-x86.pubxml"
 
-echo win-x86 no-single-file section
+ECHO win-x86 no-single-file section
 
 dotnet publish "Memenim.csproj" -c "Release" -p:PublishProfile="netcore3.1 win-x86 standalone no-single-file.pubxml"
 dotnet publish "Memenim.csproj" -c "Release" -p:PublishProfile="netcore3.1 win-x86 no-single-file.pubxml"
